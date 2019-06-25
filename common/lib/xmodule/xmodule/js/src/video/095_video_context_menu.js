afc211300ff850f85c16808521fd5f6a00ad43ed
David Adams <dcadams@stanford.edu>
2019-06-25 14:37:50 -0700

    Revert "Fix issue where refreshing page after adjusting video speed would result in video playing at 1.0x (regardless of speed selected) even though UI would show selected speed."
    This reverts commit 83f5736a1f7076f9c635a9c2a1975431f3453c22.

--- common/lib/xmodule/xmodule/js/src/video/095_video_context_menu.js
+++ common/lib/xmodule/xmodule/js/src/video/095_video_context_menu.js
@@ -216,8 +216,7 @@ function(Component) {
         },
 
         appendContent: function(content) {
-            var $content = $(content);
-            this.getElement().append($content);
+            this.getElement().append(content);
             return this;
         },
 
@@ -247,8 +246,8 @@ function(Component) {
         },
 
         open: function() {
-            var $menu = (this.isRendered) ? this.getElement() : this.populateElement();
-            this.container.append($menu);
+            var menu = (this.isRendered) ? this.getElement() : this.populateElement();
+            this.container.append(menu);
             AbstractItem.prototype.open.call(this);
             this.overlay.show(this.container);
             return this;
@@ -355,8 +354,7 @@ function(Component) {
         },
 
         show: function(container) {
-            var $elem = $(this.getElement());
-            $(container).append($elem);
+            $(container).append(this.getElement());
             this.delegateEvents();
             return this;
         },
@@ -391,10 +389,8 @@ function(Component) {
         },
 
         createElement: function() {
-            var $spanElem,
-                $listElem,
-                $element = $('<li />', {
-                class: ['submenu-item', 'menu-item', this.options.prefix + 'submenu-item'].join(' '),
+            var element = $('<li />', {
+                'class': ['submenu-item', 'menu-item', this.options.prefix + 'submenu-item'].join(' '),
                 'aria-expanded': 'false',
                 'aria-haspopup': 'true',
                 'aria-labelledby': 'submenu-item-label-' + this.id,
@@ -402,25 +398,21 @@ function(Component) {
                 'tabindex': -1
             });
 
-            $spanElem = $('<span />', {
-                id: 'submenu-item-label-' + this.id,
-                text: this.options.label
-            });
-            this.label = $spanElem.appendTo($element);
+            this.label = $('<span />', {
+                'id': 'submenu-item-label-' + this.id,
+                'text': this.options.label
+            }).appendTo(element);
 
-            $listElem = $('<ol />', {
-                class: ['submenu', this.options.prefix + 'submenu'].join(' '),
-                role: 'menu'
-            });
-
-            this.list = $listElem.appendTo($element);
+            this.list = $('<ol />', {
+                'class': ['submenu', this.options.prefix + 'submenu'].join(' '),
+                'role': 'menu'
+            }).appendTo(element);
 
             return element;
         },
 
         appendContent: function(content) {
-            var $content = $(content);
-            this.list.append($content);
+            this.list.append(content);
             return this;
         },
 
@@ -635,7 +627,7 @@ function(Component) {
                 }, {
                     label: i18n.Speed,
                     items: _.map(state.speeds, function(speed) {
-                        var isSelected = parseFloat(speed) === state.speed;
+                        var isSelected = speed === state.speed;
                         return {label: speed + 'x', callback: speedCallback, speed: speed, isSelected: isSelected};
                     }),
                     initialize: function(menuitem) {
